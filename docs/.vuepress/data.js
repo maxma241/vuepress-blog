07869cc53595f79071209134325ccfd9232df483
Max Ma <marfi241@gmail.com>
2021-09-22 03:02:22 +0800

    data: update resume data at Aotter

--- docs/.vuepress/data.js
+++ docs/.vuepress/data.js
@@ -1,28 +1,93 @@
 const introduce = {
   cname: '馬楊陞',
   ename: 'Max Ma',
-  jobTitle: 'Full-stack Developer',
+  jobTitle: 'Full-stack Engineer',
   livedIn: '台北市',
   email: 'marfi241@gmail.com',
   github: 'https://github.com/maxma241',
-  about:`熱衷程式新事物。喜歡與人討論如何解決問題，具有獨立開發，解決問題及良好的學習能力。平常會關心各種技術及技術社團的文章。`
+  about: `熱衷程式新事物。喜歡與人討論如何解決問題，具有獨立開發，解決問題及良好的學習能力。平常會關心各種技術及技術社團的文章。`,
 }
 
 const experiences = [
   {
-    workAt: '電獺公司',
+    workAt: '電獺股份有限公司',
     companyUrl: 'https://aotter.net/',
-    position: 'Front-end Engineer',
-    duration: '2020/06/15 – now',
-    description: '',
+    position: 'Front-end Engineer, Senior Software Engineer',
+    duration: '2020/06/15 - 2021/05/31, 2021/06/01 - now',
+    description: 'Ahead of Time，縮短「價值創造（Time to value）」的時間',
     highlights: [
-      'Newbie-ing'
+      '負責公司自有產品，包含 CI/CD',
+      '2021 走鐘獎網站',
+      '解決各種四面八方來的 web 技術問題( pm2, npm, CI/CD with node.js, Vue, Typescript, JavaScript ...etc )',
+      '分享過 Vue 2 to Vue 3, Typegoose, Vanilla to Vue, Git',
+      'Linode / Rancher / Azure pipeline / GitHub',
+      'Asana / Slack / Google Meet'
     ],
-    projects:[
+    projects: [
       {
-        name: 'TBD',
-        description: 'TBD',
-        tags: ['Vue Ecosystem']
+        name: '2021 走鐘獎官方網站',
+        description: '主要開發 API - 獎項提名, 會員票選, 會員搶票',
+        tags: [
+          'Nuxt',
+          'composition-api',
+          'TypeScript',
+          'ChakraUI',
+          'Express',
+          'MongoDB',
+          'Typegoose',
+          'Rancher',
+          'K8S',
+          'Azure Pipeline',
+        ],
+      },
+      {
+        name: 'Supr.Link - 短網址服務',
+        description:
+          '維護 / 新增功能，修正訂閱金流問題。為了解決技術債，大重構 API',
+        tags: [
+          'TypeScript',
+          'Buefy',
+          'Nuxt',
+          'Express',
+          'MongoDB',
+          'Typegoose',
+          'vue-class-component',
+          'jest',
+          'Rancher',
+          'K8S',
+          'Azure Pipeline',
+        ],
+      },
+      {
+        name: 'Supr.One - 創作者平台',
+        description: '維護為主，建立 Native App JavaScript bridge layer',
+        tags: ['Vuetify v1.x', 'Nuxt', 'Plyr', 'vue-class-component'],
+      },
+      {
+        name: '電獺少女官方網站',
+        description: '新增 Apple Event 直播頁面，RSS feeds API。開發 / 維護 Trek 廣告置入。',
+        tags: ['Nuxt', 'TailwindCSS', 'Plyr', 'vue-class-component'],
+      },
+      {
+        name: 'Aotter Account - SSO service',
+        description:
+          '主要負責前端，實作 sign in with apple, email verification flow',
+        tags: ['Nuxt', 'vue-class-component'],
+      },
+      {
+        name: 'CatRun / CatWalk',
+        description: '廣告呈現平台, 呈現多種類型廣告，像是圖片 / 影音 / 互動式廣告。實作新類型的廣告',
+        tags: ['Nuxt', 'Vanilla.js', 'Express', 'Ads', 'MongoDB', 'Google IMA'],
+      },
+      {
+        name: 'Trek Web SDK',
+        description: '提供 JS API ( Ads 生命週期及設定檔 )。從 Trek API 取得廣告 Payload',
+        tags: ['Nuxt', 'Vanilla.js', 'Ads', 'MongoDB'],
+      },
+      {
+        name: '鄉民晚報爬蟲',
+        description: '新建立的repository。定期爬 feedly / twitter API',
+        tags: ['node.js', 'TypeScript', 'Crawler', 'MongoDB'],
       },
     ],
   },
@@ -30,7 +95,7 @@ const experiences = [
     workAt: '趨勢科技 TrendMicro',
     companyUrl: 'https://www.trendmicro.com/',
     position: 'Software Engineer',
-    duration: '2020/02/24 – 2020/06/10',
+    duration: '2020/02/24 - 2020/06/10',
     description: '',
     highlights: [
       '負責公司內部service',
@@ -40,13 +105,23 @@ const experiences = [
       'DevOps with Jenkins',
       'Scrum, weekly meeting',
       '使用Jira',
-      '每天可能有case要做(操作PLVRS)'
     ],
-    projects:[
+    projects: [
       {
         name: 'PLVRS (Project Legal Vulnerabilities Review System)',
-        description: 'migrate 15年老web app成react SPA, pending兩年的React SPA, 整個專案大重構',
-        tags: ['GO', 'React', 'React Redux', 'React Router', 'Styled Component', 'React-use', 'TypeScript', 'TrendMicro Frontend', 'Jenkins']
+        description:
+          'migrate 15 年老 web app 成 react SPA, pending 兩年的 React SPA, 整個專案大重構',
+        tags: [
+          'GO',
+          'React',
+          'React Redux',
+          'React Router',
+          'Styled Component',
+          'React-use',
+          'TypeScript',
+          'TrendMicro Frontend',
+          'Jenkins',
+        ],
       },
     ],
   },
@@ -54,7 +129,7 @@ const experiences = [
     workAt: '香港商阿爾伊股份有限公司(RE 紅包)',
     companyUrl: 'https://myre.life/',
     position: 'Front-end Developer, Full-stack Developer',
-    duration: '2017/04 – 2017/12, 2018/01 - 2020/02',
+    duration: '2017/04 - 2017/12, 2018/01 - 2020/02',
     description: '只要登錄消費金額，管你怎麼Pay，皆享最高4%現金回饋',
     highlights: [
       '在這具有從0到有的經驗，建立店家平台、官網、內部管理平台...等等',
@@ -65,36 +140,90 @@ const experiences = [
       '以TDD(jest)建置micro service(RE序號)',
       '參與過CI/CD的建置(jenkins, gitlab ci, ELK......)，目前使用jenkins',
     ],
-    projects:[
+    projects: [
       {
         name: 'RE Boss店家平台',
-        description: '從0建置vue Web App(Native App則為hybrid)，先以MVP方式，後續持續的開發新功能及優化後台管理功能...等等',
-        tags: ['Vue', 'Vuex', 'Vue-router', 'Sass', 'RWD', 'Webpack', 'Express', 'i18n']
+        description:
+          '從0建置vue Web App(Native App則為hybrid)，先以MVP方式，後續持續的開發新功能及優化後台管理功能...等等',
+        tags: [
+          'Vue',
+          'Vuex',
+          'Vue-router',
+          'Sass',
+          'RWD',
+          'Webpack',
+          'Express',
+          'i18n',
+        ],
       },
       {
         name: 'RE Admin管理平台',
-        description: '從0建置Nuxt App(Full-stack)，包括CRM、CMS、workflow...等等，獨立開發許多功能',
-        tags: ['Nuxt', 'TypeScript', 'Sass', 'Element-UI', 'Express', 'GraphQL', 'Apollo GraphQL', 'i18n', 'MySql', 'Sequelize', 'MongoDB', 'Mongoose', 'Redis', 'GCS',]
+        description:
+          '從0建置Nuxt App(Full-stack)，包括CRM、CMS、workflow...等等，獨立開發許多功能',
+        tags: [
+          'Nuxt',
+          'TypeScript',
+          'Sass',
+          'Element-UI',
+          'Express',
+          'GraphQL',
+          'Apollo GraphQL',
+          'i18n',
+          'MySql',
+          'Sequelize',
+          'MongoDB',
+          'Mongoose',
+          'Redis',
+          'GCS',
+        ],
       },
       {
         name: 'RE官方網站',
-        description: '從0建置開發第一版Express網站，一開始主要寫活動頁面，後來脫手後被改為Nuxt',
-        tags: ['Nuxt', 'TypeScript', 'Sass', 'Express', 'i18n',]
+        description:
+          '從0建置開發第一版Express網站，一開始主要寫活動頁面，後來脫手後被改為Nuxt',
+        tags: ['Nuxt', 'TypeScript', 'Sass', 'Express', 'i18n'],
       },
       {
         name: 'RE API',
         description: '負責各種新功能開發，與同事們協同開發',
-        tags: ['Express', 'TypeScript', 'Moleculer', 'i18n', 'MySql', 'Redis', 'MongoDB', 'GCS']
+        tags: [
+          'Express',
+          'TypeScript',
+          'Moleculer',
+          'i18n',
+          'MySql',
+          'Redis',
+          'MongoDB',
+          'GCS',
+        ],
       },
       {
         name: 'RE Micro-services',
-        description: '為了國際專案，新需求以micro-service開發，開發過幣別匯率服務、MGM專屬序號、各平台之CMS、電子發票、全家便利商店資訊、會計帳務、RE序號...等等',
-        tags: ['TypeScript', 'NATS', 'Moleculer', 'i18n', 'MySql', 'TypeORM', 'Redis']
+        description:
+          '為了國際專案，新需求以micro-service開發，開發過幣別匯率服務、MGM專屬序號、各平台之CMS、電子發票、全家便利商店資訊、會計帳務、RE序號...等等',
+        tags: [
+          'TypeScript',
+          'NATS',
+          'Moleculer',
+          'i18n',
+          'MySql',
+          'TypeORM',
+          'Redis',
+        ],
       },
       {
         name: 'RE Line@ 活動ChatBot',
-        description: '一個簡單可擴充的KYC問答架構，會員完成活動後能獲得紅包現金',
-        tags: ['TypeScript', 'Bottender', 'Express', 'Molecular','TypeORM', 'MySql', 'Redis']
+        description:
+          '一個簡單可擴充的KYC問答架構，會員完成活動後能獲得紅包現金',
+        tags: [
+          'TypeScript',
+          'Bottender',
+          'Express',
+          'Molecular',
+          'TypeORM',
+          'MySql',
+          'Redis',
+        ],
       },
     ],
   },
@@ -102,21 +231,22 @@ const experiences = [
     workAt: '商智資訊',
     companyUrl: 'http://www.bi.com.tw/',
     position: 'Java Engineer',
-    duration: '2016/06 – 2017/02',
-    description: '商智資訊提供了銀行金融服務解決方案，例如財富管理系統、行動理專APP、資產配置系統。',
+    duration: '2016/06 - 2017/02',
+    description:
+      '商智資訊提供了銀行金融服務解決方案，例如財富管理系統、行動理專APP、資產配置系統。',
     highlights: [
       '負責財富管理系統',
       '幫助公司導入git版控，開了內部教育訓練課程 (<a href="https://www.slideshare.net/maxma98/git-73699918" target="_blank">slideshare</a>)',
       '傳教同事ES6, vue, react等等新奇東西',
-      '專案follow git flow 使用jenkins自動包版'
+      '專案follow git flow 使用jenkins自動包版',
     ],
-    projects:[
+    projects: [
       {
         name: 'WMS財富管理系統(Wealth Management System)',
         description: '維護及開發新功能',
-        tags: ['Java', 'Struts2', 'Spring', 'MSSQL', 'JSP', 'jQuery']
+        tags: ['Java', 'Struts2', 'Spring', 'MSSQL', 'JSP', 'jQuery'],
       },
-    ]
+    ],
   },
   // {
   //   workAt: '基隆某家高中補習班',
@@ -134,15 +264,15 @@ const educations = [
   {
     educatedAt: '國立台灣海洋大學',
     title: '資工所碩士',
-    duration: '2013/08 – 2015/08',
+    duration: '2013/08 - 2015/08',
     description: '服務導向暨軟體工程實驗室(SOSELab)',
   },
   {
     educatedAt: '國立台灣海洋大學',
     title: '資工系學士',
-    duration: '2009/09 – 2013/06',
-    description: '主要走Web領域'
-  }
+    duration: '2009/09 - 2013/06',
+    description: '主要走Web領域',
+  },
 ]
 // Ud4b544877ee32adeb15a4bd735209962
 const projects = [
@@ -150,50 +280,51 @@ const projects = [
     title: '個人網站',
     meta: ['vuepress', 'markdown'],
     description: '把履歷跟技術文章整合在這',
-    url:'',
-    repository: 'https://github.com/maxma241/vuepress-blog'
+    url: '',
+    repository: 'https://github.com/maxma241/vuepress-blog',
   },
   {
     title: '技術文章',
     meta: ['github', 'issue', 'markdown'],
     description: '不定期會開issue寫技術文章',
-    url:'',
-    repository: 'https://github.com/maxma241/tech-murmur/issues'
+    url: '',
+    repository: 'https://github.com/maxma241/tech-murmur/issues',
   },
   {
     title: '個人簡歷',
     meta: ['vue', 'vue-cli 2.x', 'vue-material', 'ES6', 'scss'],
     description: '用了vue寫了個簡歷頁面，使用vue material',
-    url:'https://maxma241.github.io/',
-    repository: 'https://github.com/maxma241/maxma241.github.io'
+    url: 'https://maxma241.github.io/',
+    repository: 'https://github.com/maxma241/maxma241.github.io',
   },
   {
     title: '為了下指令打卡',
     meta: ['node', 'selenium-webdriver', 'babel', 'ES6'],
     description: '為了省時間打卡就做了這個',
-    url:'',
-    repository: 'https://github.com/maxma241/work_punch'
+    url: '',
+    repository: 'https://github.com/maxma241/work_punch',
   },
   {
     title: '初學React',
     meta: ['react'],
-    description: '將以前用jQuery寫的專案，改為React (兩個youtube channel皆已消失)',
-    url:'http://maxma241.github.io/tvprogram/TVRE.html',
-    repository: 'https://github.com/maxma241/tvprogram'
+    description:
+      '將以前用jQuery寫的專案，改為React (兩個youtube channel皆已消失)',
+    url: 'http://maxma241.github.io/tvprogram/TVRE.html',
+    repository: 'https://github.com/maxma241/tvprogram',
   },
 ]
 
 const skills = [
-  { name: 'JavaScript', value:90 },
-  { name: 'HTML/CSS', value:70 },
-  { name: 'ES6', value:87 },
-  { name: 'Vue.js', value:70 },
+  { name: 'JavaScript', value: 90 },
+  { name: 'HTML/CSS', value: 70 },
+  { name: 'ES6', value: 87 },
+  { name: 'Vue.js', value: 70 },
   { name: 'React.js', value: 60 },
-  { name: 'Node.js', value: 50},
-  { name: 'Webpack', value: 80},
-  { name: 'Git', value:90 },
-  { name: 'Git/Github flow', value:87 },
-  { name: 'Gulp', value:60 }
+  { name: 'Node.js', value: 50 },
+  { name: 'Webpack', value: 80 },
+  { name: 'Git', value: 90 },
+  { name: 'Git/Github flow', value: 87 },
+  { name: 'Gulp', value: 60 },
 ]
 
 export default {
@@ -201,5 +332,5 @@ export default {
   experiences,
   educations,
   projects,
-  skills
+  skills,
 }
--- docs/.vuepress/en-data.js
+++ docs/.vuepress/en-data.js
@@ -1,28 +1,89 @@
 const introduce = {
   cname: '馬楊陞',
   ename: 'Max Ma',
-  jobTitle: 'Full-stack Developer',
+  jobTitle: 'Full-stack Engineer',
   livedIn: 'Taipei, Taiwan',
   email: 'marfi241@gmail.com',
   github: 'https://github.com/maxma241',
-  about: `Enthusiastic about new techs, currently rolling in the JS ecosystem. I like to discuss how to solve problems with people, with independent development, problem solving and good learning ability. I usually care about articles from various technical and technical communities.`
+  about: `Enthusiastic about new techs, currently rolling in the JS ecosystem. I like to discuss how to solve problems with people, with independent development, problem solving and good learning ability. I keep following up the articles from various technical and technical communities.`,
 }
 
 const experiences = [
   {
     workAt: 'Aotter Inc.',
     companyUrl: 'https://aotter.net/',
-    position: 'Front-end Engineer',
-    duration: '2020/06/15 – now',
-    description: '',
+    position: 'Front-end Engineer, Senior Software Engineer',
+    duration: '2020/06/15 - 2021/05/31, 2021/06/01 - now',
+    description: 'Ahead of Time，縮短「價值創造（Time to value）」的時間',
     highlights: [
-      'Newbie-ing'
+      'Responsibility to All Web Applications of Company, including SA, SD, CI/CD',
+      '2021 Walk-bell awards Web Application',
+      'Provide solutions for Web Tech from everywhere ( pm2, npm, CI/CD with node.js, Vue, Typescript, JavaScript ...etc)',
+      'Tech sharing - Vue 2 to Vue 3, Typegoose, Vanilla to Vue, Git',
+      'Linode / Rancher / Azure pipeline / Github',
     ],
-    projects:[
+    projects: [
       {
-        name: 'TBD',
-        description: 'TBD',
-        tags: ['Vue Ecosystem']
+        name: '2021 Walk-bell awards Web Application',
+        description: 'Develop the RESTful API - Account, Award nominations, Voting, Tickets. Deploy to Rancher (K8S) with Azure Pipeline',
+        tags: [
+          'Nuxt',
+          'composition-api',
+          'TypeScript',
+          'ChakraUI',
+          'Express',
+          'MongoDB',
+          'Typegoose',
+          'Rancher',
+          'K8S',
+          'Azure Pipeline',
+        ],
+      },
+      {
+        name: 'Supr.Link - Shorten URL Platform',
+        description:
+          'Link Tags feature. Fix the Payment API (ECPay). Big Refactoring ( Rewriting ).',
+        tags: [
+          'TypeScript',
+          'Buefy',
+          'Nuxt',
+          'Express',
+          'MongoDB',
+          'Typegoose',
+          'vue-class-component',
+          'jest',
+        ],
+      },
+      {
+        name: 'Supr.One - Creator Platform',
+        description: 'Maintenance. Create "Native App JavaScript bridge layer"',
+        tags: ['Vuetify v1.x', 'Nuxt', 'Plyr', 'vue-class-component'],
+      },
+      {
+        name: 'Aotter Girls Web Application',
+        description: 'Apple Event Page. RSS feeds API. invoke Ads from Trek SDK',
+        tags: ['Nuxt', 'TailwindCSS', 'Plyr', 'vue-class-component'],
+      },
+      {
+        name: 'Aotter Account - SSO service',
+        description:
+          'Develop "Sign in with apple". Email verification flow.',
+        tags: ['Nuxt'],
+      },
+      {
+        name: 'CatRun / CatWalk',
+        description: 'Ads Display Service. Provide multiple categories Ads, like Images, VAST, interaction',
+        tags: ['Nuxt', 'Vanilla.js', 'Express', 'Ads', 'MongoDB', 'Google IMA'],
+      },
+      {
+        name: 'Trek Web SDK ( Ads Invoker )',
+        description: 'Provide JS API about Ads Config and lifecycle. Fetch Ads Payloads from trek API',
+        tags: ['Nuxt', 'Vanilla.js', 'Ads', 'Web SDK'],
+      },
+      {
+        name: 'Pnn Crawler',
+        description: 'new repository. crawl from feedly / twitter API',
+        tags: ['node.js', 'TypeScript', 'Crawler', 'MongoDB'],
       },
     ],
   },
@@ -30,7 +91,7 @@ const experiences = [
     workAt: 'TrendMicro',
     companyUrl: 'https://www.trendmicro.com/',
     position: 'Software Engineer',
-    duration: '2020/02 – now',
+    duration: '2020/02 - now',
     description: '',
     highlights: [
       'Responsible for internal services',
@@ -40,13 +101,24 @@ const experiences = [
       'Learning DevOps with Jenkins',
       'Scrum (weekly meeting)',
       'Use Jira',
-      'Maybe do the case operations everyday on the old PLVRS'
+      'do the case operations everyday on the old PLVRS',
     ],
     projects: [
       {
         name: 'PLVRS (Project Legal Vulnerabilities Review System)',
-        description: 'migrate 15 years old web app to new React SPA(the SPA has been pending for 2 years), Refactor and Improve architecture',
-        tags: ['GO', 'React', 'React Redux', 'React Router', 'Styled Component', 'React-use', 'TypeScript', 'TrendMicro Frontend', 'Jenkins']
+        description:
+          'migrate 15 years old web app to new React SPA(the SPA has been pending for 2 years), Refactor and Improve architecture',
+        tags: [
+          'GO',
+          'React',
+          'React Redux',
+          'React Router',
+          'Styled Component',
+          'React-use',
+          'TypeScript',
+          'TrendMicro Frontend',
+          'Jenkins',
+        ],
       },
     ],
   },
@@ -55,7 +127,8 @@ const experiences = [
     companyUrl: 'https://myre.life/',
     position: 'Front-end Developer, Full-stack Developer',
     duration: '2017/04 – 2017/12, 2018/01 - 2020/02',
-    description: 'By entering the payment amount, up to 4% cash rebate for you, no matter how you pay',
+    description:
+      'By entering the payment amount, up to 4% cash rebate for you, no matter how you pay',
     highlights: [
       'Having projects experience from 0 to 1, like RE-Boss, Official Web Site, RE Admin(CRM, CMS)...etc',
       'Participate in product feature development and planning, and co-work with other front-end and back-end colleagues, like UI/UX, Database design, micro services...etc',
@@ -68,33 +141,88 @@ const experiences = [
     projects: [
       {
         name: 'RE Boss',
-        description: 'Created vue Web App(hybrid on Native App) from zero, MVP first, then, keep developing and optimizing features',
-        tags: ['Vue', 'Vuex', 'Vue-router', 'Sass', 'RWD', 'Webpack', 'Express', 'i18n']
+        description:
+          'Created vue Web App(hybrid on Native App) from zero, MVP first, then, keep developing and optimizing features',
+        tags: [
+          'Vue',
+          'Vuex',
+          'Vue-router',
+          'Sass',
+          'RWD',
+          'Webpack',
+          'Express',
+          'i18n',
+        ],
       },
       {
         name: 'RE Admin',
-        description: 'Created Nuxt App(Full-stack) from zero, including CRM, CMS, WorkFlow...etc. Develop many features independently',
-        tags: ['Nuxt', 'TypeScript', 'Sass', 'Element-UI', 'Express', 'GraphQL', 'Apollo GraphQL', 'i18n', 'MySql', 'Sequelize', 'MongoDB', 'Mongoose', 'Redis', 'GCS',]
+        description:
+          'Created Nuxt App(Full-stack) from zero, including CRM, CMS, WorkFlow...etc. Develop many features independently',
+        tags: [
+          'Nuxt',
+          'TypeScript',
+          'Sass',
+          'Element-UI',
+          'Express',
+          'GraphQL',
+          'Apollo GraphQL',
+          'i18n',
+          'MySql',
+          'Sequelize',
+          'MongoDB',
+          'Mongoose',
+          'Redis',
+          'GCS',
+        ],
       },
       {
         name: 'RE Official Web site',
-        description: 'Created express.js web first. mainly develop the activity page at the beginning. Later, it was changed to Nuxt web.',
-        tags: ['Nuxt', 'TypeScript', 'Sass', 'Express', 'i18n',]
+        description:
+          'Created express.js web first. mainly develop the activity page at the beginning. Later, it was changed to Nuxt web.',
+        tags: ['Nuxt', 'TypeScript', 'Sass', 'Express', 'i18n'],
       },
       {
         name: 'RE API',
-        description: 'Responsible for new feature development, collaborative development with colleagues',
-        tags: ['Express', 'TypeScript', 'Molecular', 'i18n', 'MySql', 'Redis', 'MongoDB', 'GCS']
+        description:
+          'Responsible for new feature development, collaborative development with colleagues',
+        tags: [
+          'Express',
+          'TypeScript',
+          'Molecular',
+          'i18n',
+          'MySql',
+          'Redis',
+          'MongoDB',
+          'GCS',
+        ],
       },
       {
         name: 'RE Micro-services',
-        description: 'For international projects, new requirements are developed by microservices. developed many services, like currency exchange rate service, MGM serial number, CMS for each platform, electronic invoice, family convenience store information, accounting, RE serial number...etc.',
-        tags: ['TypeScript', 'NATS', 'Molecular', 'i18n', 'MySql', 'TypeORM', 'Redis']
+        description:
+          'For international projects, new requirements are developed by microservices. developed many services, like currency exchange rate service, MGM serial number, CMS for each platform, electronic invoice, family convenience store information, accounting, RE serial number...etc.',
+        tags: [
+          'TypeScript',
+          'NATS',
+          'Molecular',
+          'i18n',
+          'MySql',
+          'TypeORM',
+          'Redis',
+        ],
       },
       {
         name: 'RE Line@ ChatBot',
-        description: 'A simple and extensible KYC Q&A structure. Member will get RE coin reward after the Q&A.',
-        tags: ['TypeScript', 'Bottender', 'Express', 'Molecular', 'TypeORM', 'MySql', 'Redis']
+        description:
+          'A simple and extensible KYC Q&A structure. Member will get RE coin reward after the Q&A.',
+        tags: [
+          'TypeScript',
+          'Bottender',
+          'Express',
+          'Molecular',
+          'TypeORM',
+          'MySql',
+          'Redis',
+        ],
       },
     ],
   },
@@ -103,19 +231,20 @@ const experiences = [
     companyUrl: 'http://www.bi.com.tw/',
     position: 'Java Engineer',
     duration: '2016/06 – 2017/02',
-    description: 'BI provides banking financial services solutions such as wealth management systems, mobile technology apps, and asset allocation systems.',
+    description:
+      'BI provides banking financial services solutions such as wealth management systems, mobile technology apps, and asset allocation systems.',
     highlights: [
       'Responsible for wealth management system (WMS), using Java Struts2, Spring',
       'Help companies import git version control and opened internal education training courses (<a href="https://www.slideshare.net/maxma98/git-73699918" target="_blank">slideshare</a>)',
       'Sharing New thins with colleague, like ES6, react, vue, with colleague',
-      'Using Jenkins, and git flow'
-    ]
+      'Using Jenkins, and git flow',
+    ],
   },
   // {
   //   workAt: '基隆某家高中補習班',
   //   companyUrl: '',
   //   position: '數理輔導老師',
-  //   duration: '2009/09 – 2011/01',
+  //   duration: '2009/09 - 2011/01',
   //   description: '高中數理還不錯，因緣際會的在這邊打工',
   //   highlights: [
   //     '教數學、物理、化學'
@@ -126,16 +255,16 @@ const experiences = [
 const educations = [
   {
     educatedAt: 'National Taiwan Ocean University',
-    title: 'Computer Science master\'s degree',
-    duration: '2013/08 – 2015/08',
+    title: "Computer Science master's degree",
+    duration: '2013/08 - 2015/08',
     description: 'Service-Oriented and Soft Engineer Lab(SOSELab)',
   },
   {
     educatedAt: 'National Taiwan Ocean University',
     title: 'Computer Science bachelor degree',
-    duration: '2009/09 – 2013/06',
-    description: 'major Web knowledge'
-  }
+    duration: '2009/09 - 2013/06',
+    description: 'major Web knowledge',
+  },
 ]
 // Ud4b544877ee32adeb15a4bd735209962
 const projects = [
@@ -144,35 +273,36 @@ const projects = [
     meta: ['vuepress', 'markdown'],
     description: 'Write some tech articles, and resume',
     url: 'https://maxma241.github.io/vuepress-blog/',
-    repository: 'https://github.com/maxma241/vuepress-blog'
+    repository: 'https://github.com/maxma241/vuepress-blog',
   },
   {
     title: 'Tech murmur',
     meta: ['github', 'issue', 'markdown'],
     description: 'Irregularly write tech articles in issues',
     url: '',
-    repository: 'https://github.com/maxma241/tech-murmur/issues'
+    repository: 'https://github.com/maxma241/tech-murmur/issues',
   },
   {
     title: 'Old resume',
     meta: ['vue', 'vue-cli', 'vue-material', 'ES6', 'scss'],
     description: 'Create resume by vue and vue-material',
     url: 'https://maxma241.github.io/',
-    repository: 'https://github.com/maxma241/maxma241.github.io'
+    repository: 'https://github.com/maxma241/maxma241.github.io',
   },
   {
     title: 'for auto punch',
     meta: ['node', 'selenium-webdriver', 'babel', 'ES6'],
     description: 'I did this to punch in order to save time.',
     url: '',
-    repository: 'https://github.com/maxma241/work_punch'
+    repository: 'https://github.com/maxma241/work_punch',
   },
   {
     title: 'React Beginner',
     meta: ['react'],
-    description: 'Refactor old jQuery project to React.(the two YouTube channels are disabled)',
+    description:
+      'Refactor old jQuery project to React.(the two YouTube channels are disabled)',
     url: 'http://maxma241.github.io/tvprogram/TVRE.html',
-    repository: 'https://github.com/maxma241/tvprogram'
+    repository: 'https://github.com/maxma241/tvprogram',
   },
 ]
 
@@ -186,7 +316,7 @@ const skills = [
   { name: 'Webpack', value: 80 },
   { name: 'Git', value: 90 },
   { name: 'Git/Github flow', value: 87 },
-  { name: 'Gulp', value: 60 }
+  { name: 'Gulp', value: 60 },
 ]
 
 export default {
@@ -194,5 +324,5 @@ export default {
   experiences,
   educations,
   projects,
-  skills
+  skills,
 }
